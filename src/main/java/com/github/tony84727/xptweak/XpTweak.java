0574c7a8b6f84309b34bcf9bba42c5a1a1e207b5
Tony Duan <tony84727@gmail.com>
2021-03-21 00:36:41 +0800

    cleanup code

--- src/main/java/com/github/tony84727/xptweak/XpTweak.java
+++ src/main/java/com/github/tony84727/xptweak/XpTweak.java
@@ -1,37 +1,13 @@
 package com.github.tony84727.xptweak;
 
-import discord4j.common.util.Snowflake;
-import discord4j.core.DiscordClient;
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.channel.Channel;
-import discord4j.core.object.presence.Activity;
-import discord4j.core.object.presence.Status;
-import discord4j.discordjson.json.gateway.ImmutableStatusUpdate;
-import discord4j.gateway.GatewayClient;
-import discord4j.rest.entity.RestChannel;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.ServerChatEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import reactor.core.publisher.Mono;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(XpTweak.MOD_ID)
@@ -53,6 +29,4 @@ public class XpTweak {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-//    public void onChecking(ModL)
 }
